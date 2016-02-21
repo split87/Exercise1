9b1dc3490f7d19798e04cf4ff9aa27b378d4d126
split <split@virtual-gfx.de>
2016-02-21 11:57:04 +0100

    added class

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,14 @@
+
+public class HelloUser {
+
+	private String name;
+	
+	public HelloUser(String name) {
+		this.name = name;
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello, " + name);
+	}
+	
+}
